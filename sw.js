01063f1198eab8165193a3e7e8f118e36cff9959
tttosnown <162364812+tttosnown@users.noreply.github.com>
2026-09-13 23:12:20 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="futurism-v4";
-const ASSETS=["./","./index.html","./manifest.json","./icons/icon-192.png","./icons/icon-512.png"];
+const CACHE="future-v19.14";
+const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png","./future-logo.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
 self.addEventListener("fetch",e=>{
